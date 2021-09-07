b6842f92f714071cdfc1a3572fe2f6ee3db46f8d
BerBac <bernhard.ebersbach@gmail.com>
2021-09-07 21:01:57 +0200

    Sea moth -> Seamoth

--- DisableCrosshairSN/CrosshairMenu.cs
+++ DisableCrosshairSN/CrosshairMenu.cs
@@ -26,7 +26,7 @@ namespace DisableCrosshairSN
         public static void AddGerneralTab_Postfix(uGUI_OptionsPanel __instance)
         {
             __instance.AddHeading(0, "Crosshair");
-            __instance.AddToggleOption(0, "Disable crosshair in sea moth", Config.NoCrosshairInSeaMoth, (bool v) => Config.NoCrosshairInSeaMoth = v);
+            __instance.AddToggleOption(0, "Disable crosshair in seamoth", Config.NoCrosshairInSeaMoth, (bool v) => Config.NoCrosshairInSeaMoth = v);
             __instance.AddToggleOption(0, "Disable crosshair in prawn suit", Config.NoCrosshairInPrawnSuit, (bool v) => Config.NoCrosshairInPrawnSuit = v);
             __instance.AddToggleOption(0, "Disable crosshair completely", Config.DisableCrosshair, (bool v) => Config.DisableCrosshair = v);
         }
